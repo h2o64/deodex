7db68c7cd8f815b7d624168be93a25b87553dacb
Louis Popi <theh2o64@gmail.com>
2015-07-20 15:04:36 +0200

    Initial release

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Credits :
+- oat2dex https://github.com/testwhat/SmaliEx
+- smali/baksmali https://code.google.com/p/smali/
+- Tech N You http://forum.xda-developers.com/android/software-hacking/guide-android-l-5-0-lollipop-deodex-t2967242
---
+++ deodex.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+mkdir -p deodex
+mkdir -p NEW/$1
+cp $1/$1.apk .
+cp $1/arm/$1.odex .
+java -jar oat2dex.jar boot $1.odex
+java -jar baksmali-2.0.6.jar -a 22 -x dex/$1.dex -o deodex
+java -jar smali-2.0.6.jar -a 22 deodex -o classes.dex
+7za u -tzip $1.apk classes.dex
+mv $1.apk NEW/$1/
+rm classes.dex
+rm -rf deodex
+rm -rf odex
+rm -rf dex
