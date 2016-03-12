6b7f125964213bae58c2128eacc4766d2555a407
Louis Popi <theh2o64@gmail.com>
2016-03-12 22:12:27 +0100

    deodex: Update all the JARs

--- deodex_low.sh
+++ deodex_low.sh
@@ -4,8 +4,8 @@ mkdir -p NEW/$1
 cp $1/$1.apk .
 cp $1/arm/$1.odex .
 java -jar oat2dex.jar odex $1.odex
-java -jar baksmali-2.0.6.jar -a 22 -x $1.dex -o deodex
-java -jar smali-2.0.6.jar -a 22 deodex -o classes.dex
+java -jar baksmali-2.1.1.jar -a 22 -x $1.dex -o deodex
+java -jar smali-2.1.1.jar -a 22 deodex -o classes.dex
 7za u -tzip $1.apk classes.dex
 mv $1.apk NEW/$1/
 rm classes.dex
